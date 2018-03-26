f6b282afe0e0a24c2b1d4cc599423fd1ce40db04
sanghoon.han2 <sanghoon.han2@worksmobile.com>
2018-03-26 19:46:22 +0900

    PR test

--- app/src/main/java/com/worksmobile/android/botproject/ChatRoomListActivity.java
+++ app/src/main/java/com/worksmobile/android/botproject/ChatRoomListActivity.java
@@ -2,6 +2,7 @@ package com.worksmobile.android.botproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ChatRoomListActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class ChatRoomListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room_list);
+        Log.d("test","풀리퀘 테스트입니다");
     }
 }
