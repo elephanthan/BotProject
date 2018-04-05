77cf1786fd3166e75d2c57543e5792084bb4f352
sanghoon.han2 <sanghoon.han2@worksmobile.com>
2018-04-05 13:12:39 +0900

    UserinfoDialog를 DialogFragment로 변경 - 그에따른 이벤트 함수도 변경

---
+++ app/src/main/java/com/worksmobile/android/botproject/view/dialog/UserinfoDialogFragment.java
@@ -0,0 +1,27 @@
+package com.worksmobile.android.botproject.view.dialog;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.worksmobile.android.botproject.R;
+
+/**
+ * Created by user on 2018. 4. 5..
+ */
+
+public class UserinfoDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_userinfo, container);
+
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        return view;
+    }
+}
