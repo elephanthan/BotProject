bde0ed4ccf6083117f6bef8b72b5a23c4470ed19
sanghoon.han2 <sanghoon.han2@worksmobile.com>
2018-04-10 20:12:11 +0900

    API 설계 토대로 작성중..

--- app/src/main/java/com/worksmobile/android/botproject/api/ApiService.java
+++ app/src/main/java/com/worksmobile/android/botproject/api/ApiService.java
@@ -1,13 +1,30 @@
 package com.worksmobile.android.botproject.api;
 
 
+import com.worksmobile.android.botproject.model.Chatroom;
+import com.worksmobile.android.botproject.model.User;
+
+import java.util.List;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("comments")
     Call<ResponseBody> getComment(@Query("postId") int postId);
+
+    @POST("login")
+    Call<User> loginUser(@Body User user);
+
+    @GET("chatrooms")
+    Call<List<Chatroom>> getChatroomListByUserId(@Path("userId") String userId);
+
+    @GET("users")
+    Call<User> getUser(@Query("userId") String userId);
 
 }
--- app/src/main/java/com/worksmobile/android/botproject/util/ApiUtils.java
+++ app/src/main/java/com/worksmobile/android/botproject/util/ApiUtils.java
@@ -5,7 +5,8 @@ import com.worksmobile.android.botproject.api.RetrofitClient;
 
 public class ApiUtils {
 
-    public static final String BASE_URL = "http://jsonplaceholder.typicode.com/";
+    //public static final String BASE_URL = "http://jsonplaceholder.typicode.com/";
+    public static final String BASE_URL = "http://{serverAddress}/api/v1/";
 
     public static ApiService getApiService() {
         return RetrofitClient.getClient(BASE_URL).create(ApiService.class);
