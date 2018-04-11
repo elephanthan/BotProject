2dd27f8e838439f1fc67f87bd8f66ad439f6bdc1
sanghoon.han2 <sanghoon.han2@worksmobile.com>
2018-04-11 11:16:33 +0900

    API Prototyping 완료

--- app/src/main/java/com/worksmobile/android/botproject/api/ApiService.java
+++ app/src/main/java/com/worksmobile/android/botproject/api/ApiService.java
@@ -2,17 +2,23 @@ package com.worksmobile.android.botproject.api;
 
 
 import com.worksmobile.android.botproject.model.Chatroom;
+import com.worksmobile.android.botproject.model.Message;
 import com.worksmobile.android.botproject.model.User;
 
 import java.util.List;
+import java.util.Map;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 public interface ApiService {
     @GET("comments")
@@ -22,9 +28,37 @@ public interface ApiService {
     Call<User> loginUser(@Body User user);
 
     @GET("chatrooms")
-    Call<List<Chatroom>> getChatroomListByUserId(@Path("userId") String userId);
+    Call<List<Chatroom>> getChatroomsByUserId(@Query("userId") String userId);
+
+    @GET("chatrooms/{chatroomId}")
+    Call<Chatroom> getChatroom(@Path("chatroomId") String chatroomId);
+
+    @POST("chatrooms")
+    Call<Chatroom> createChatroom(@Body Chatroom chatroom);
+
+    @PATCH("chatrooms/{chatroomId}")
+    Call<Chatroom> updateChatroom(@Body Map<String, String> map);
+
+    @POST("user_chatrooms/{chatroomId}")
+    Call<List<User>> inviteUsers(@Path("chatroomId")String chatroomId, @Body List<User> users);
+
+    @DELETE("user_chatrooms/{chatroomId}")
+    Call<User> leaveChatroom(@Path("chatroomId") String chatroomId, @Body User user);
+
+    @GET("users/{userId}")
+    Call<User> getUser(@Path("userId") String userId);
 
     @GET("users")
-    Call<User> getUser(@Query("userId") String userId);
+    Call<User> getUsers(@QueryMap User user);
+
+
+    @PUT("users/{userId}")
+    Call<User> updateUser(@Path("userId") String userId, @Body User user);
+
+    @GET("messages")
+    Call<List<Message>> getMessages(@QueryMap Message message);
+
+    @POST("bots")
+    Call<List<Map>> sendBotEvent(@Body List<Map> maps);
 
 }
