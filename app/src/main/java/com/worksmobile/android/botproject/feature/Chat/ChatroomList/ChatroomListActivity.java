1a1e8a6513e48912037a8b989084dc51fa9bc277
sanghoon.han2 <sanghoon.han2@worksmobile.com>
2018-04-13 13:45:23 +0900

    비콘 관련 작업 - 비콘 sdk 의존성 추가 - 비콘 샘플 코드를 이용하여 ChatroomListActivity에 진입하면 비콘 기록을 파일에 쓰드록하는 서비스를 실행시킴 - 메뉴에서 + 버튼을 누르면 해당 파일을 보여주는 액티비티를 임시적으로 만들어놓음

--- app/src/main/java/com/worksmobile/android/botproject/feature/Chat/ChatroomList/ChatroomListActivity.java
+++ app/src/main/java/com/worksmobile/android/botproject/feature/Chat/ChatroomList/ChatroomListActivity.java
@@ -8,11 +8,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.worksmobile.android.botproject.feature.MySetting.MySettingActivity;
-import com.worksmobile.android.botproject.feature.Chat.NewChatting.NewChattingActivity;
+import com.facebook.stetho.Stetho;
 import com.worksmobile.android.botproject.R;
-import com.worksmobile.android.botproject.model.Chatroom;
+import com.worksmobile.android.botproject.beacon.RecoBackgroundRangingService;
 import com.worksmobile.android.botproject.feature.Chat.Chatroom.ChatroomLab;
+import com.worksmobile.android.botproject.feature.Chat.NewChatting.NewChattingActivity;
+import com.worksmobile.android.botproject.feature.MySetting.MySettingActivity;
+import com.worksmobile.android.botproject.model.Chatroom;
 
 import java.util.List;
 
@@ -24,13 +26,22 @@ public class ChatroomListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Stetho.initializeWithDefaults(this);
+
         setContentView(R.layout.activity_chatroom_list);
 
         chatroomRecyclerView = (RecyclerView) findViewById(R.id.chat_room_recycler_view);
         chatroomRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        Intent intent = new Intent(this, RecoBackgroundRangingService.class);
+        startService(intent);
+        //stopService(new Intent(this, RecoBackgroundRangingService.class));
+
         updateUI();
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -45,7 +56,9 @@ public class ChatroomListActivity extends AppCompatActivity {
             case R.id.menu_item_new_chatting:
                 //Toast.makeText(getApplicationContext(),"New Chatting Menu item was selected.", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(this, NewChattingActivity.class));
+
                 return true;
+
             case R.id.menu_item_my_setting:
                 //Toast.makeText(getApplicationContext(),"My Setting Menu item was selected.", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(this, MySettingActivity.class));
--- app/src/main/java/com/worksmobile/android/botproject/feature/Chat/NewChatting/NewChattingActivity.java
+++ app/src/main/java/com/worksmobile/android/botproject/feature/Chat/NewChatting/NewChattingActivity.java
@@ -1,15 +1,43 @@
 package com.worksmobile.android.botproject.feature.Chat.NewChatting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import com.worksmobile.android.botproject.R;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class NewChattingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_newchatting);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        try {
+            // 파일에서 읽은 데이터를 저장하기 위해서 만든 변수
+            StringBuffer data = new StringBuffer();
+            FileInputStream fis = openFileInput("mybeacon.txt");//파일명
+            BufferedReader buffer = new BufferedReader
+                    (new InputStreamReader(fis));
+            String str = buffer.readLine();                 // 파일에서 한줄을 읽어옴
+            String str2 = str;
+            while (str != null) {
+                data.append(str + "\n");
+                str = buffer.readLine();
+                str2 += str;
+            }
+            textView.setText(str2);
+            buffer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
