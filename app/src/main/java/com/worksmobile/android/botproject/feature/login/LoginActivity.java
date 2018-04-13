036b40a2a5b6a85bba88340e89299022835da353
sanghoon.han2 <sanghoon.han2@worksmobile.com>
2018-04-13 19:17:32 +0900

    login-v1.1 branch Start

---
+++ app/src/main/java/com/worksmobile/android/botproject/feature/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.worksmobile.android.botproject.feature.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.worksmobile.android.botproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
